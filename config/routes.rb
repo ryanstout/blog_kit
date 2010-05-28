80b0b1c640c75205fe31342c7dddb8accde442e8
Ryan Stout <ryanstout@gmail.com>
2010-05-28 17:09:46 -0600

    Move generators under lib

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :blog_posts, :collection => {:drafts => :any}, :has_many => :blog_comments
-
+if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+	# Rails 3
+	Rails.application.routes.draw do |map|
+		resources :blog_posts do
+			resource :blog_comments
+			
+			collection do
+				get :drafts
+			end
+		end
+	end
+else
+	# Rails 2.x
+	ActionController::Routing::Routes.draw do |map|
+	  map.resources :blog_posts, :collection => {:drafts => :any}, :has_many => :blog_comments
+	end
 end
--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require File.dirname(__FILE__) + "/lib/blog_kit"
 
 # Setup blog kit
-BlogKit.instance
+# BlogKit.instance
