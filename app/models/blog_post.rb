9436fc54a7c4c7447922152611511b50d6873ef1
Ryan Stout <ryanstout@gmail.com>
2010-06-02 19:53:57 -0600

    Html safe tags

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -33,7 +33,11 @@ class BlogPost < ActiveRecord::Base
 	end
 
 	def tags_with_links
-		self.tags.split(/,/).collect {|t| "<a href=\"/blog_posts/tag/#{t.strip}\">#{t.strip}</a>" }.join(', ')
+		html = self.tags.split(/,/).collect {|t| "<a href=\"/blog_posts/tag/#{t.strip}\">#{t.strip}</a>" }.join(', ')
+		# Rails 3
+		return html.html_safe if html.respond_to?(:html_safe)
+		# Rails 2.x
+		return html
 	end
 	
 	def save_tags
