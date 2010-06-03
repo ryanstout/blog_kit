14dcbd62a4a89161ff050f8e15878314dd3637eb
Ryan Stout <ryanstout@gmail.com>
2010-06-02 19:52:05 -0600

    Add rails 3 generator instructions

--- install.rb
+++ install.rb
@@ -8,17 +8,21 @@ begin
 		rails_root = File.join(File.dirname(__FILE__), '..', '..', '..')
 	end
 
-
 	if (File.exists?(rails_root) && File.exists?(File.join(rails_root, 'app')))
 		require "#{rails_root}/config/boot"
 		require "#{rails_root}/config/environment"
 		require 'rails_generator'
 		require 'rails_generator/scripts/generate'
 	end
-	
-	# Run the generator
-	Rails::Generator::Scripts::Generate.new.run(['blog_assets'])
-	
+
+	begin
+		# Run the generator
+		Rails::Generator::Scripts::Generate.new.run(['blog_assets'])
+
+	rescue Exception => e
+		puts "The following error ocurred: " + e.inspect
+	end
 rescue Exception => e
-	puts "The following error ocurred: " + e.inspect
+	puts "BlogKit is installed, to continue run:"
+	puts "rails generate blog_assets"
 end
